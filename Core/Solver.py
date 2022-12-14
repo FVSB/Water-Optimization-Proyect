59134acf1ad1858468c7f4fd3cf7fb99fe891d42
Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>
2022-12-13 23:57:19 -0500

    Soluciones numericas
    Signed-off-by: Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>

--- Core/Solver.py
+++ Core/Solver.py
@@ -3,7 +3,7 @@ from Core.Auxiliar_Class import Process
 
 
 def Solve(process_1: Process, process_2: Process):
-    # Declaracion de variables
+    # Declaración de variables
 
     Water_From_1_to_2 = process_1.Process_To_send[(
         process_2.Company, process_2.Name)][0]
@@ -25,15 +25,15 @@ def Solve(process_1: Process, process_2: Process):
     # Invocar una nuevo problema del solver de pulp (minimizar)
     prob = pl.LpProblem("Water", pl.LpMinimize)
 
-    # X: cant de agua que se envia de 1 a 2
+    # X: cant de agua que se envía de 1 a 2
     x = pl.LpVariable(str("Water From "+str(process_1.Company)+str(process_1.Name)+" To "+str(process_2.Company)+str(process_2.Name)), lowBound=0,
                       upBound=Water_From_1_to_2, cat="Continuous")
 
-    # Y: cant de agua que se envia de 2 a 1
+    # Y: cant de agua que se envía de 2 a 1
     y = pl.LpVariable(str("Water From "+str(process_2.Company)+str(process_2.Name)+" To "+str(process_1.Company)+str(process_1.Name)), lowBound=0,
                       upBound=Water_From_2_to_1, cat="Continuous")
 
-    # Lider
+    # Líder
     Leader_Supply = pl.LpVariable(
         "Leader_Water_Supply", lowBound=0, upBound=Water_Leader_supply, cat="Continuous")
     Fresh_Water = Leader_Water_price*Leader_Supply
@@ -47,9 +47,9 @@ def Solve(process_1: Process, process_2: Process):
     # Objective Function
     obj = float(process_1.time_of_test) * \
         (Fresh_Water+Discharge_Price+Transport_Price)
-    # Agragar la funcion objetivo al problema
+    # Agregar la función objetivo al problema
     prob += obj, "obj"
-    # Agragar consicion al problema
+    # Agregar condición al problema
     prob += Leader_Supply >= 0 and Leader_Supply <= float(
         process_1.Cant_Water), "c1"
     prob += float(process_2.C_Max_Out) <= float(process_1.C_Max_In), "c2"
